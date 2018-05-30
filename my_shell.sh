54210f9bd3ef5b11de0a098a8f5ba38b1f1e7e94
xiuyuanjun <729125866@qq.com>
2018-05-30 08:57:58 +0800

    update

--- my_shell.sh
+++ my_shell.sh
@@ -5,9 +5,21 @@ correct_apps=(`cat /usr/local/etc/scripts/app_first_run.txt`)
 apps=(`cat /usr/local/etc/scripts/appname_path.txt | awk -F ' ' '{print $NF}'`)
 app_status_path="/usr/local/etc/scripts/app_status.txt"
 cat /dev/null > $app_status_path
-for app in ${apps[*]}
+for app in ${correct_apps[*]}
 do
-    if [[ "${correct_apps[*]}" =~ "$app" ]]
+    if [ `grep $app /usr/local/etc/scripts/appname_path.txt | wc -l` -gt 0 ]
+    then
+        echo 1
+    else
+        echo 0
+    fi
+done
+
+app_status_path="/usr/local/etc/scripts/app_status.txt"
+cat /dev/null > $app_status_path
+for app in ${correct_apps[*]}
+do
+    if [ `grep $app /usr/local/etc/scripts/appname_path.txt | wc -l` -gt 0 ]
     then
         echo "$app" 1 >> $app_status_path
     else
