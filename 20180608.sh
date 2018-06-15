a83e8510431850079a229b3f481b89f5e9c0cf54
xiuyuanjun <729125866@qq.com>
2018-06-15 14:33:16 +0800

    update

--- 20180608.sh
+++ 20180608.sh
@@ -17,7 +17,17 @@ netstat -tlnp|grep 80 #查看端口号占用情况。
 #如何判断文件是否有重复的行？------ sort和uniq搞起
 sort a.txt | uniq -d
 #其中， uniq -d是输出duplicate了的行， 也就是重复的行。 要注意， 这是建立在sort后的基础之上的。
+#uniq [ -c | -d | -u ] [ -f Fields ] [ -s Characters ] [ -Fields ] [ +Characters ] [ InFile [ OutFile ] ]
+ #-c 在输出行前面加上每行在输入文件中出现的次数。
+#　　-d 仅显示重复行。
+#　　-f  忽略由 Fields 变量指定的字段数目。如果 Fields 变量的值超过输入行中的字段数目, uniq 命令用空字符串进行比较。这个标志和 -Fields 标志是等价的。
+#　　-u 仅显示不重复的行。
 
+#例子：查询一个文件重复最多的前10条记录：
+uniq -c file | sort -n -r |head -10
+
+#查询2个文件重复的数据
+ cat file1 file2 |sort|uniq -d
 
 #用grep -c来统计匹配的行数
 #grep -c 的作用类似grep | wc -l，不同的是，如果是查找多个文件，grep -c会统计每个文件匹配的行数，每行一个文件的列出来，而wc -l 则列出总的统计数字。
