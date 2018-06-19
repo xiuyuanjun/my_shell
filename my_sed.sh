c8068bece35ef7ab11683bbb778ff44dedd90b7d
xiuyuanjun <729125866@qq.com>
2018-06-19 17:55:46 +0800

    update

--- my_sed.sh
+++ my_sed.sh
@@ -6,3 +6,14 @@ sed -i '1d' filename
 sed -i 'nd' filename
 #删除最后一行
 sed -i '$d' filename
+#删除匹配行 删除以a开头的行
+sed -i '/^a*/d' tmp.txt
+#替换匹配行
+sed -i 's/^a.*/haha/g' tmp.txt
+
+#显示文件X行命令
+sed -n 'xp' filename
+
+#显示文件X行到Y行的内容
+sed -n 'x,yp' filename
+
