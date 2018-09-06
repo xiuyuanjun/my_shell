9b7e3002a8a9de0099cce5477edc3f2b42512c3e
xiuyuanjun <729125866@qq.com>
2018-09-06 01:57:30 +0000

    Update my_grep.sh

--- my_grep.sh
+++ my_grep.sh
@@ -2,3 +2,8 @@
 
 #匹配以数字开头的行
 cat file | grep -P '^\d'
+
+grep 精确匹配
+如果是有进程名为abcd,abcde等有包含"abc"字符的，那么判断将会不准备，如果精确的匹配到abc呢，当时不知道，只能很诚实的回答，不清楚
+其实答案很简单，用grep –w "abc" 或者是grep "\<abc\>"都可以实现
+-w, --word-regexp         强制 PATTERN 仅完全匹配字词
