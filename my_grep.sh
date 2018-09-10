2d0835bf7678f951d83101981bc3e123c5ef4d1a
xiuyuanjun <729125866@qq.com>
2018-09-10 00:22:57 +0000

    Update my_grep.sh

--- my_grep.sh
+++ my_grep.sh
@@ -7,3 +7,4 @@ grep 精确匹配
 如果是有进程名为abcd,abcde等有包含"abc"字符的，那么判断将会不准备，如果精确的匹配到abc呢，当时不知道，只能很诚实的回答，不清楚
 其实答案很简单，用grep –w "abc" 或者是grep "\<abc\>"都可以实现
 -w, --word-regexp         强制 PATTERN 仅完全匹配字词
+-w, --word-regexp         force PATTERN to match only whole words
