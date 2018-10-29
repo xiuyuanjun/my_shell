ed7961b39c2fd5e34345a2f38315c4a5ab997757
xiuyuanjun <729125866@qq.com>
2018-10-29 14:59:34 +0800

    Update my_grep.sh

--- my_grep.sh
+++ my_grep.sh
@@ -8,3 +8,7 @@ grep 精确匹配
 其实答案很简单，用grep –w "abc" 或者是grep "\<abc\>"都可以实现
 -w, --word-regexp         强制 PATTERN 仅完全匹配字词
 -w, --word-regexp         force PATTERN to match only whole words
+
+grep -C 5 foo file 显示file文件里匹配foo字串那行以及上下5行
+grep -B 5 foo file 显示foo及前5行
+grep -A 5 foo file 显示foo及后5行
