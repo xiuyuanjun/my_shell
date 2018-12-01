88ad9f29c3a0ba3416aebd81d5305f21716d9ce9
xiuyuanjun <729125866@qq.com>
2018-12-01 13:05:28 +0800

    Update my_find.sh

--- my_find.sh
+++ my_find.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 #linux 使用find命令查找包含某字符串的文件
 find .|xargs grep -ri "要查找的字符串"
+
+#删除匹配的文件
+find ./ -name test.txt -delete
+find ./ -name test.txt | xargs rm -f
+find ./ -type f -name txt.txt -delete
