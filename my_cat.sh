7c5cd57f5886f4dd97518b9e6cf34694ec2e1c1a
xiuyuanjun <729125866@qq.com>
2018-12-01 12:53:15 +0800

    Update my_cat.sh

--- my_cat.sh
+++ my_cat.sh
@@ -37,3 +37,34 @@ cat /dev/fd0 > OUTFILE
 相反的，如果想把 image file 写到软盘，输入：
 
 cat IMG_FILE > /dev/fd0
+
+
+
+cat --help
+Usage: cat [OPTION]... [FILE]...
+Concatenate FILE(s), or standard input, to standard output.
+
+  -A, --show-all           equivalent to -vET
+  -b, --number-nonblank    number nonempty output lines, overrides -n
+  -e                       equivalent to -vE
+  -E, --show-ends          display $ at end of each line
+  -n, --number             number all output lines
+  -s, --squeeze-blank      suppress repeated empty output lines
+  -t                       equivalent to -vT
+  -T, --show-tabs          display TAB characters as ^I
+  -u                       (ignored)
+  -v, --show-nonprinting   use ^ and M- notation, except for LFD and TAB
+      --help     display this help and exit
+      --version  output version information and exit
+
+With no FILE, or when FILE is -, read standard input.
+
+Examples:
+  cat f - g  Output f's contents, then standard input, then g's contents.
+  cat        Copy standard input to standard output.
+
+Report cat bugs to bug-coreutils@gnu.org
+GNU coreutils home page: <http://www.gnu.org/software/coreutils/>
+General help using GNU software: <http://www.gnu.org/gethelp/>
+Report cat translation bugs to <http://translationproject.org/team/>
+For complete documentation, run: info coreutils 'cat invocation'
